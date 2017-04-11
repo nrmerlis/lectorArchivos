4d228bdd6f89633a3a660a70a4598024deb2c690
nrmerlis <nicoxeneize.10@gmail.com>
2017-04-11 13:32:36 -0300

    Varias modificaciones para que funcione

--- lectorArchivosGenerico.c
+++ lectorArchivosGenerico.c
@@ -2,32 +2,44 @@
 #include <getnum.h>
 #include <stdlib.h>
 #include <string.h>
+int fileRead (char *,char[100][100]);
 
-int main (void){
+typedef struct fileData fileDataType;
+
+void trimArrayValues(char configValues[100][100],fileDataType *);
+
+ struct fileData{
+        char *name;
+        int *value;
+        };
 
 
-fileRead("testFile.txt");
+int main(void){
+
+    char pepe[100][100];
+
+
+    fileDataType data[10];
+    int a = fileRead("testFile.txt",pepe);
+
+    trimArrayValues(pepe,data);
+
 
 }
 
-void fileRead (char * path)
-{
+int fileRead (char * path,char configValues[100][100]){
 
 FILE * file;
-
-char configValues[100][100];
-
-/*configValues = malloc(100);
-*configValues = malloc(100);*/
 
 file = fopen(path,"r"); /*Le asigno al puntero file la ruta del archivo que abro con la funcion fopens*/
 int i=0;
     if (file == NULL){
 
-        printf("\nError de apertura del archivo. \n\n");
+        return(1);
     }else{
-    while (feof(file) == 0)
+    while (feof(file)==0)
         {
+
                 fgets(configValues[i],100,file);
 
                 ++i;
@@ -35,47 +47,52 @@ int i=0;
 
 
          strcpy(configValues[i], "END");
-       //char *eq=strrchr(configValues[i-1],'=');
+
 
 
 
 }
+    return i;
 }
 // char *eq=strrchr(configValues[i],'=');
-void trimArrayValues(char **configValues){
+void trimArrayValues(char configValues[100][100],fileDataType data[10]){
 int i=0;
-struct fileData {
-        char *name;
-        int *value;
-        };
 
-struct fileData data[10];
+
 char *row;
 char *name;
 while (strcmp(configValues[i],"END")){
 
     row=strrchr(configValues[i],'='); //Puntero a '=' de cada linea
 
-    int nameLength=0;
+    int nameLength= (strlen(configValues[i])-1)-(strlen(row)-1);
 
-    (data+i)->value = row+1; //Guardo el valor de cada linea desde el =
+    (data+i)->value = malloc(strlen(row));
+
+    memmove((data+i)->value,row+1,strlen(row)); //Guardo el valor de cada linea desde el =
 
 
-    nameLength = (strlen(configValues[i])-1) - (strlen(row)-1);
 
-    name = malloc(nameLength);
+     name = malloc(nameLength);
 
     memmove(name,configValues[i],nameLength);
 
-    name[nameLength]='\0';
+    memmove((name+nameLength),"\0",1);
 
-   (data+i)->name = name;
 
-    free(name);
+    (data+i)->name = malloc(nameLength);
+
+    memmove((data+i)->name,name,nameLength+1);
+
+
+    //free(name);
 
     ++i;
 
+
+
 }
+return;
 }
 
 
